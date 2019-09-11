73778c61bff31b5326e250cf85c7e8ca92266d56
Vinicius Trevisan <vinicius_trev@hotmail.com>
2019-09-11 08:42:40 -0300

    Comentarios Códigos VM

--- VM/mainWindow.js
+++ VM/mainWindow.js
@@ -1,7 +1,13 @@
+/* Máquina Virtual - Execução
+ * Bruno Vicente Donaio Kitaka - 16156341
+ * João Pedro Porta - 16039778
+ * Vinicius Trevisan - 16011231
+ */
+
 /* Variáveis globais para manipulação de instruções */
-let memoria = [];   /* Variavel que representa a regiao de memoria (memoria = M[]) */
+let memoria = [];        /* Variavel que representa a regiao de memoria (memoria = M[]) */
 let stackPointer = -1;   /* Ponteiro da região de STACK (stackPointer = s) */
-let pc = 0;         /* Program Counter */
+let pc = 0;              /* Program Counter */
 let pcAnterior = 0;
 let debugFlag = 0;
 let step = false;
@@ -11,52 +17,42 @@ let statusBreakpoint = 0
 /**
  * Função executada para reiniciar a execucao da VM
  */
-function reset()
-{
+function reset() {
   window.location.reload(true);  /* Recarrega a página ao clicar no botão reset */
 }
 
 /**
  * Função executada quando clicar no botão PLAY
  */
-function rodarCodigo() 
-{ 
+function rodarCodigo() {
   let stackWindow = document.getElementById("stack");  /* Obtem o objeto para escrever na GUI de stack */
-  while(pc < codigo.length)                                   /* Para cada linha do codigo */
-  {
+  while (pc < codigo.length)                                   /* Para cada linha do codigo */ {
     let line = codigo[pc]
     let elementos = line.trim().split(" ")              /* Remove os espacos extras do fim e começo e quebra a linha a cada espaco armanzenando cada elemento na variavel elemento*/
     let funcao = elementos.shift()                      /* Recupera e remove o primeiro elemento da linha (instrução ou label) */
-    let linhaAnterior = document.getElementById("line-"+pcAnterior);
-    let linha = document.getElementById("line-"+pc)     /* Pega o objeto que representa a linha que esta sendo executada */
+    let linhaAnterior = document.getElementById("line-" + pcAnterior);
+    let linha = document.getElementById("line-" + pc)     /* Pega o objeto que representa a linha que esta sendo executada */
     pcAnterior = pc;
 
-    if(debugFlag == 1)                                  /* Se a flag de debug for igual a 1, indica que o código esta sendo executado com a opção de debug */
-    {
+    if (debugFlag == 1)                                  /* Se a flag de debug for igual a 1, indica que o código esta sendo executado com a opção de debug */ {
       linhaAnterior.classList.remove("highlight");      /* Remove o highlight da linha */
       linha.classList.add("highlight");                 /* Aplica a classe highlight a linha que esta sendo executada */
-      if(breakpoints[pc] == true)                       /* Se existir um breakpoint ativo */
-      {
-       statusBreakpoint = 1;                            /* Ativa a flag para execução com breakpoint */
+      if (breakpoints[pc] == true)                       /* Se existir um breakpoint ativo */ {
+        statusBreakpoint = 1;                            /* Ativa a flag para execução com breakpoint */
       }
 
-      if(statusBreakpoint == 1)                         /* Caso a flag de breakpoint ainda estiver ativa, continua com o step */
-      {
-        if(step == false)                               /* Após executar a instrução parada sai do while */
-        {
+      if (statusBreakpoint == 1)                         /* Caso a flag de breakpoint ainda estiver ativa, continua com o step */ {
+        if (step == false)                               /* Após executar a instrução parada sai do while */ {
           return;
         }
-        else
-        {
+        else {
           step = false;
         }
       }
     }
 
-    elementos.forEach((e, index, arr) =>                /* Para cada elemento restante, ou seja, argumentos ou a instrução NULL */
-    {
-      if (e.length != 0)                                /* Se não existirem argumentos */
-      {
+    elementos.forEach((e, index, arr) =>                /* Para cada elemento restante, ou seja, argumentos ou a instrução NULL */ {
+      if (e.length != 0)                                /* Se não existirem argumentos */ {
         if (e.match(/^[0-9]+$/) == null) {              /* Se elemento nao for um numero (ou seja, um parametro), o armazaena na tabela pois eh um label */
           arr[index] = tabela[e];                       /* Armazena o Label na tabela */
         }
@@ -66,24 +62,19 @@ function rodarCodigo()
       }
     })
 
-    if (!(funcao in tabela))                            /* Se a função nao for label, ou seja, é uma instrução, executa a funcao passando os respectivos parametros */
-    {
-      if(funcao === "RD")                               /* Se a função lida for READ */
-      {
+    if (!(funcao in tabela))                            /* Se a função nao for label, ou seja, é uma instrução, executa a funcao passando os respectivos parametros */ {
+      if (funcao === "RD")                               /* Se a função lida for READ */ {
         /* Garante que o popup só pegara numeros inteiros */
-        do
-        {
+        do {
           var answer = parseInt(prompt("Digite o valor de INPUT: "))  /* Abre um pop up para inserir o dado */
-        }while(isNaN(answer));                                        /* Garante que o dado sempre será um numero inteiro */
+        } while (isNaN(answer));                                        /* Garante que o dado sempre será um numero inteiro */
 
-        if(answer != "")                                /* Se o dado inserido não for vazio (ou seja, inseriu algo valido *****TRATAR NUMEROS*****) */
-        {
+        if (answer != "")                                /* Se o dado inserido não for vazio (ou seja, inseriu algo valido *****TRATAR NUMEROS*****) */ {
           instrucoes["RD"](+answer)                     /* Executa a função de READ */
         }
       }
-      else
-      {
-        if(funcao === "HLT")                             /* Termina a execução do programa ao encontrar um HLT, saí do while */
+      else {
+        if (funcao === "HLT")                             /* Termina a execução do programa ao encontrar um HLT, saí do while */
           return
         else
           instrucoes[funcao](...elementos)               /* Caso a função NÃO FOR READ, executa ela normal passando os argumentos */
@@ -105,20 +96,17 @@ function rodarCodigo()
   }
 }
 
-function ativarDebug()  /* Acionada quando clicar no botão de debug */
-{
+function ativarDebug()  /* Acionada quando clicar no botão de debug */ {
   debugFlag = 1;  /* Quando executar o código com a opção debug, seta a variavel global para 1 */
   rodarCodigo();  /* E roda o código */
 }
 
-function setStep()     /* Acionada quando clicar no botão de step */
-{
+function setStep()     /* Acionada quando clicar no botão de step */ {
   step = true;    /* Avisa que o step esta ativo */
   rodarCodigo();  /* Executa o código */
 }
 
-function setContinue() /* Acionada quando clicar no botão de continue */
-{
+function setContinue() /* Acionada quando clicar no botão de continue */ {
   step = false;           /* Desativa o modo step */
   statusBreakpoint = 0;   /* Desativa o modo breakpoint */
   rodarCodigo();          /* E roda o código */
--- VM/openFile.js
+++ VM/openFile.js
@@ -1,33 +1,37 @@
+/* Máquina Virtual - Editor de Texto
+ * Bruno Vicente Donaio Kitaka - 16156341
+ * João Pedro Porta - 16039778
+ * Vinicius Trevisan - 16011231
+ */
+
 /* Variáveis Globais da VM */
 let codigo = [];    /* Variavel onde em cada posicao representa uma linha do codigo */
 let tabela = {};    /* Tabela labels x linha do codigo, utilizada para jumps */
 let breakpoints = {};
 
 /* Render Code Window ->> Handler ativado quando importar o código OBJ/TXT */
-function openFile(){
-    var input = document.createElement('input');
-    input.type = 'file';
+function openFile() {
+  var input = document.createElement('input');
+  input.type = 'file';
 
-    input.onchange = e => { 
-        // getting a hold of the file reference
-        var file = e.target.files[0]; 
-     
-        // setting up the reader
-        var reader = new FileReader();
-        reader.readAsText(file,'UTF-8');
-     
-        // here we tell the reader what to do when it's done reading...
-        reader.onload = readerEvent => 
-        {
-           var content = readerEvent.target.result; // this is the content!
-           montarTexto(content);
-        }
-     }
-    input.click();
+  input.onchange = e => {
+    // getting a hold of the file reference
+    var file = e.target.files[0];
+
+    // setting up the reader
+    var reader = new FileReader();
+    reader.readAsText(file, 'UTF-8');
+
+    // here we tell the reader what to do when it's done reading...
+    reader.onload = readerEvent => {
+      var content = readerEvent.target.result; // this is the content!
+      montarTexto(content);
+    }
+  }
+  input.click();
 }
 
-function montarTexto(code)                          /* Recebe o conteudo do arquivo (parametro code) e o trata nessa arrow function (Callback) */
-{
+function montarTexto(code)                          /* Recebe o conteudo do arquivo (parametro code) e o trata nessa arrow function (Callback) */ {
   const lines = code.split("\n");                   /* Quebra o conteudo do arquivo em linhas separadas a partir do \n */
   div = document.querySelector(".code-window");     /* Instanciacao do objeto que recebera o conteudo do arquivo - div onde irá aparecer o codigo bruto */
   div.innerHTML = "";                               /* Limpando o conteudo do objeto antes de carregar o codigo nesse objeto */
@@ -35,23 +39,21 @@ function montarTexto(code)                          /* Recebe o conteudo do arqu
   ol.classList.add("code-list");                    /* Adiciona uma classe no objeto HTML para o funcionamento do CSS */
   div.appendChild(ol)                               /* Adiciona a lista ordenada na div para exibir o codigo obj */
   let numLinha = 1                                  /* Inicia contagem da linha de texto como 1 */
-  lines.forEach((line, index, array) =>             /* Para cada linha do codigo */
-  {
+  lines.forEach((line, index, array) =>             /* Para cada linha do codigo */ {
     let clickou = false;
 
     /* Cria o dicionário de Labels */
-    if (line.includes('NULL'))                      /* Se existe o parametro NULL, eh label */
-    {    
+    if (line.includes('NULL'))                      /* Se existe o parametro NULL, eh label */ {
       tabela[line.split(' ')[0]] = numLinha - 1;    /* Salva o numero da linha - 1 na tabela (-1 para executar a proxima instrução)  */
     }
-    
+
     /*
      * Render code
      * Adicionando elemetos HTML para estetica e
      * renderizaçao do codigo
     **/
     const li = document.createElement("li");        /* Instanciando um item para ser inserido na lista de itens */
-    li.setAttribute("id", "line-"+index);
+    li.setAttribute("id", "line-" + index);
 
     /* Cria uma div para cada linha (cada uma dessas divs é composta por uma região cinza - esquerda - e uma região branca - direita) */
     const divBreak = document.createElement("div")  /* Cria a barra lateral esquerda para o botão de breakpoint*/
@@ -69,25 +71,22 @@ function montarTexto(code)                          /* Recebe o conteudo do arqu
 
     /* BOTÃO */
     const breakButton = document.createElement("button");
-    breakButton.setAttribute("id", "break-"+index);
+    breakButton.setAttribute("id", "break-" + index);
     breakButton.innerHTML = "<i class='fas fa-circle select-trans'></i>";
-    breakButton.addEventListener("click",function()
-    {
+    breakButton.addEventListener("click", function () {
       numLinha = this.id.split("-")[1];
-      if(clickou === false)
-      {
+      if (clickou === false) {
         /* insere tabela o numero da linha */
         breakpoints[numLinha] = true;
         breakButton.innerHTML = "<i class='fas fa-circle break-div'></i>"
         clickou = true;
-      } 
-      else
-      {
+      }
+      else {
         /* remove tabela  */
         breakpoints[numLinha] = false;
         breakButton.innerHTML = "<i class='fas fa-circle select-trans'></i>";
         clickou = false;
-      }  
+      }
     });
     divBreak.appendChild(breakButton);
 
@@ -99,7 +98,7 @@ function montarTexto(code)                          /* Recebe o conteudo do arqu
     const lineText = document.createTextNode(line)    /* Cria um const (variavel) para armazenar o código (linha a linha) */
     divLine.appendChild(lineText)                     /* Escreve na divLine o código */
 
-    breakpoints[numLinha-1] = false;                  /* Armazena onde tem breakpoints */
+    breakpoints[numLinha - 1] = false;                  /* Armazena onde tem breakpoints */
 
     numLinha++;                                       /* Incrementa o número da linha */
 
