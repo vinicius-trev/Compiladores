bafb0a9b78748e3c2dcf94d3dede9e56d0d0560d
Vinicius Trevisan <vinicius_trev@hotmail.com>
2019-09-11 10:05:14 -0300

    Import js Front

--- Compilador/Lexico/funcoesTratamento.js
+++ Compilador/Lexico/funcoesTratamento.js
@@ -1,24 +1,29 @@
-/* Função responsável por tratar dígitos */
+/* Função responsável por tratar tokens que formam dígitos 0-9 */
 function trataDigito() {
 
 }
 
+/* Função responsável por tratar tokens que formam identificadores e palavras reservadas a-Z considerando _ */
 function tratarIdentificadorPalavraReservada() {
 
 }
 
+/* Função responsável por tratar tokens que formam simbolos de atribuição : := */
 function tratarAtribuicao() {
 
 }
 
+/* Função responsável por tratar tokens que formam operadores aritmeticos + - *  */
 function tratarOperadorAritmetico() {
 
 }
 
+/* Função responsável por tratar tokens que formam operadores relacionais < <= > >= != */
 function tratarOperadorRelacional() {
 
 }
 
+/* Função responsável por tratar tokens que formam pontuação . , ; ( ) */
 function tratarPontuação() {
 
 }
