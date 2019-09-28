37c2540f9fbc7a306a5867f286aa0fdd80e8662d
Vinicius Trevisan <vinicius_trev@hotmail.com>
2019-09-28 15:16:39 -0300

    Sintatico: Arquivos Iniciais e protótipos de funções

---
+++ Compilador/Sintatico/funcoesTratamento.js
@@ -0,0 +1,79 @@
+function analisaBloco() {
+
+}
+
+function analisaEtVariaveis() {
+
+}
+
+function analisaVariaveis() {
+
+}
+
+function analisaTipo() {
+
+}
+
+function analisaComandos() {
+
+}
+
+function analisaComandosSimples() {
+
+}
+
+function analisaAtribuicaoProcedimento() {
+
+}
+
+function analisaLeia() {
+
+}
+
+function analisaEscreva() {
+
+}
+
+function analisaEnquanto() {
+
+}
+
+function analisaSe() {
+
+}
+
+function analisaSubrotinas() {
+
+}
+
+function analisaDeclaracaoProcedimento() {
+
+}
+
+function analisaDeclaracaoFuncao() {
+
+}
+
+function analisaExpressao() {
+
+}
+
+function analisaExpressaoSimples() {
+
+}
+
+function analisaTermo() {
+
+}
+
+function analisaFator() {
+
+}
+
+function analisaChamadaProcedimento() {
+
+}
+
+function analisaChamadaFuncao() {
+
+}
